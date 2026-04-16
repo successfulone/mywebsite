e8945f1246115fb995fa6ce676e111c7341c342c
Ogunli Success <successfulogunli@gmail.com>
2026-04-16 18:16:37 -0400

    add login system and dashboard

--- app.py
+++ app.py
@@ -1,17 +1,38 @@
-from flask import Flask, render_template
-import os
+from flask import Flask, render_template, request, redirect, session
 
 app = Flask(__name__)
+app.secret_key = "secretkey"
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    if "user" in session:
+        return redirect("/dashboard")
+    return redirect("/login")
 
-@app.route("/about")
-def about():
-    return render_template("about.html")
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        matric = request.form.get("matric")
 
+        if matric:  # simple check
+            session["user"] = matric
+            return redirect("/dashboard")
+
+    return render_template("login.html")
+
+@app.route("/dashboard")
+def dashboard():
+    if "user" not in session:
+        return redirect("/login")
+
+    return render_template("dashboard.html")
+
+@app.route("/logout")
+def logout():
+    session.pop("user", None)
+    return redirect("/login")
 
 if __name__ == "__main__":
+    import os
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
